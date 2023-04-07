53d242777c57c8cfc8d997412c2e157db3754080
tomioka279 <axcellongnham@gmail.com>
2023-04-07 17:59:29 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello ad',
+    introDesc: `Mình có điều này muốn hỏi ad,ad phải trả lời thật lòng nha .-.`,
+    btnIntro: 'oke',
+    title: 'Ad đã có bạn trai chưa',
+    desc: 'Ad không trả lời hoặc thoát ra, tức là ad chưa có hả :> ',
+    btnYes: 'Chưa nha',
+    btnNo: 'Mình có rồi hihi',
+    question:'Ad làm bạn gái mình nhé ',
+    btnReply: 'Trả lời mình điiii ',
+    reply: 'Oke nè',
+    mess: 'Love ad 3000 :3',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/profile.php?id=100033750859649'
+}
